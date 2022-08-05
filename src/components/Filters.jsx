9eb752d056ffb66493c890d8c4aa61d9907a86fe
João Lee <joaumlimaum@gmail.com>
2022-08-05 03:21:58 -0300

    Requisito 7 concluído - requisito 6 concluído no ultimo commit também

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -27,46 +27,80 @@ function Filters() {
     const remaingOptions = updatedOptions.filter((option) => option !== selectedOption);
     setUpdatedOptions(remaingOptions);
   }
-  return (
-    <form>
-      <select
-        name="column"
-        value={ filterFields.column }
-        onChange={ handleChange }
-        data-testid="column-filter"
-      >
-        {updatedOptions.map((option) => (
-          <option key={ option }>{option}</option>))}
-      </select>
-      <select
-        data-testid="comparison-filter"
-        name="comparison"
-        value={ filterFields.comparison }
-        onChange={ handleChange }
-      >
-        <option>maior que</option>
-        <option>menor que</option>
-        <option>igual a</option>
-      </select>
-      <label htmlFor="number-input">
-        <input
-          data-testid="value-filter"
-          id="number-input"
-          name="value"
-          value={ filterFields.value }
-          onChange={ handleChange }
-          type="number"
-        />
-        <button
-          onClick={ updatePlanets }
-          data-testid="button-filter"
-          type="button"
-        >
-          FILTER
-        </button>
-      </label>
-    </form>
 
+  function removeSpecificFilter(column) {
+    setUpdatedOptions([...updatedOptions, column]);
+    const deleteSpecificFilter = filterByNumericValues
+      .filter((filter) => filter.column !== column);
+    setFilterByNumericValues(deleteSpecificFilter);
+  }
+  function removeAllFilters() {
+    setFilterByNumericValues([]);
+    setUpdatedOptions(columnOptions);
+  }
+  return (
+    <>
+      <form>
+        <select
+          name="column"
+          value={ filterFields.column }
+          onChange={ handleChange }
+          data-testid="column-filter"
+        >
+          {updatedOptions.map((option) => (
+            <option key={ option }>{option}</option>))}
+        </select>
+        <select
+          data-testid="comparison-filter"
+          name="comparison"
+          value={ filterFields.comparison }
+          onChange={ handleChange }
+        >
+          <option>maior que</option>
+          <option>menor que</option>
+          <option>igual a</option>
+        </select>
+        <label htmlFor="number-input">
+          <input
+            data-testid="value-filter"
+            id="number-input"
+            name="value"
+            value={ filterFields.value }
+            onChange={ handleChange }
+            type="number"
+          />
+          <button
+            onClick={ updatePlanets }
+            data-testid="button-filter"
+            type="button"
+          >
+            FILTER
+          </button>
+        </label>
+      </form>
+      {filterByNumericValues
+        .map(({ column, comparison, value }) => (
+          <div
+            data-testid="filter"
+            key={ column }
+          >
+            <p>{`${column} ${comparison} ${value}`}</p>
+            <button
+              type="button"
+              onClick={ () => removeSpecificFilter(column) }
+            >
+              X
+            </button>
+          </div>))}
+      <button
+        type="button"
+        data-testid="button-remove-filters"
+        onClick={ removeAllFilters }
+      >
+        Remover todas as filtragens
+
+      </button>
+    </>
   );
 }
 
